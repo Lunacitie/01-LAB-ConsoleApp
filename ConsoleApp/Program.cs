0da65728327b354b2a0f1033927529512c809a8a
nstrangar <nastrang@vsite.hr>
2025-10-21 20:46:37 +0200

    Riješen zadatak 1.

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -5,6 +5,12 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            Proizvod proizvod1 = new Proizvod("Laptop", 1200.00m, 10);
+            Proizvod proizvod2 = new Proizvod("Televizija", 800.00m, 25);
+
+            proizvod1.PrikaziInformacije();
+            proizvod2.PrikaziInformacije();
         }
     }
 }
---
+++ ConsoleApp/Proizvod.cs
@@ -0,0 +1,28 @@
+﻿namespace ConsoleApp
+{
+    public class Proizvod
+    {
+        // Svojstva (properties)
+        public string Naziv { get; set; }
+        public decimal Cijena { get; set; }
+        public int DostupnaKolicina { get; set; }
+
+        // Konstruktor
+        public Proizvod(string naziv, decimal cijena, int dostupnaKolicina)
+        {
+            Naziv = naziv;
+            Cijena = cijena;
+            DostupnaKolicina = dostupnaKolicina;
+        }
+
+        // Metoda za ispis informacija o proizvodu
+        public void PrikaziInformacije()
+        {
+            Console.WriteLine("———————————————");
+            Console.WriteLine($"Naziv: {Naziv}");
+            Console.WriteLine($"Cijena: {Cijena} Eura");
+            Console.WriteLine($"Dostupna količina: {DostupnaKolicina}");
+        }
+    }
+
+}
